3ec9f9c7bf54cab9308f016ec22c5b8d3b3b8a41
pipash <pipash.06@gmail.com>
2023-06-14 07:56:41 +0600

    exploring cloud

---
+++ src/main/java/com/demo/gateway/controller/FallbackController.java
@@ -0,0 +1,18 @@
+package com.demo.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+    @RequestMapping("/service1FallBack")
+    public Mono<String> service1FallBack() {
+        return Mono.just("Service 1 is down, Please try again later!");
+    }
+
+    @RequestMapping("/service2FallBack")
+    public Mono<String> service2FallBack() {
+        return Mono.just("Service 2 is down, Please try again later!");
+    }
+}
